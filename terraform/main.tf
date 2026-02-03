7a0b45aad5f2476e91045de0fcac1493c12ecd22
DogNose <dog.nose.rc@gmail.com>
2026-02-03 23:15:54 +0900

    2. mysqlとnetworkの追加
    mysqlに接続できるように対応

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,19 @@ provider "docker" {
   # ローカルDockerに接続
 }
 
+locals {
+  environment      = "development"
+  db_root_password = "root"
+  db_name          = "development_db"
+  db_user          = "development_user"
+  db_password      = "development_password"
+}
+
+# ネットワーク
+resource "docker_network" "this" {
+  name = "${local.environment}_network"
+}
+
 # Dockerイメージのビルド
 resource "docker_image" "flask_app" {
   name = "flask-app:latest"
@@ -23,10 +36,18 @@ resource "docker_image" "flask_app" {
   }
 }
 
+resource "docker_image" "mysql" {
+  name = "mysql:8.4"
+}
+
 # Dockerコンテナの起動
 resource "docker_container" "flask_app" {
   name  = "flask-app"
   image = docker_image.flask_app.image_id
+
+  networks_advanced {
+    name = docker_network.this.name
+  }
 
   ports {
     internal = 8000
@@ -34,8 +55,29 @@ resource "docker_container" "flask_app" {
   }
 
   env = [
-    "ENVIRONMENT=development",
+    "ENVIRONMENT=${local.environment}",
     "API_PORT=8000",
+    "DB_HOST=db",
+    "DB_PORT=3306",
+    "DB_NAME=${local.db_name}",
+    "DB_USER=${local.db_user}",
+    "DB_PASSWORD=${local.db_password}",
+  ]
+}
+
+resource "docker_container" "mysql" {
+  name  = "db"
+  image = docker_image.mysql.image_id
+
+  networks_advanced {
+    name = docker_network.this.name
+  }
+
+  env = [
+    "MYSQL_ROOT_PASSWORD=${local.db_root_password}",
+    "MYSQL_DATABASE=${local.db_name}",
+    "MYSQL_USER=${local.db_user}",
+    "MYSQL_PASSWORD=${local.db_password}",
   ]
 }
 
